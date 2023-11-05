3697f4b4df26e1692e2f779ed345ced443ad6336
Gibson Melo <gibson.melo.silva2@gmail.com>
2023-11-05 10:50:54 -0300

    Projeto Criado

---
+++ src/main/java/com/projetoGibsonSistemaDeEstoque/SistemaDeEstoquePatrimonialApplication.java
@@ -0,0 +1,13 @@
+package com.projetoGibsonSistemaDeEstoque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaDeEstoquePatrimonialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SistemaDeEstoquePatrimonialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projetoGibsonSistemaDeEstoque/SistemaDeEstoquePatrimonialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projetoGibsonSistemaDeEstoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaDeEstoquePatrimonialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
